bc54ae63c8362584c2204a631465a26df621b902
HeeJung <8253jang@daum.net>
2024-06-25 09:38:40 +0900

    second commit

--- src/main/java/web/mvc/config/WebMvcConfig.java
+++ src/main/java/web/mvc/config/WebMvcConfig.java
@@ -18,7 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
      registry.addMapping("/**")
      //.allowedOrigins("http://54.180.80.113:3000")
-             .allowedOrigins("http://54.180.80.113:9000")
+             .allowedOrigins("http://localhost:3000")
      .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
   }
 }
