f7a77b7ec9d5fcbfc63f649e9db91ef2b1fa7876
HeeJung <8253jang@daum.net>
2024-06-25 10:14:48 +0900

    test파일수정

--- src/test/java/web/mvc/SpringSecurityJwtApplicationTests.java
+++ src/test/java/web/mvc/SpringSecurityJwtApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import web.mvc.domain.Board;
 import web.mvc.domain.Member;
+import web.mvc.repository.BoardRepository;
 import web.mvc.repository.MemberRepository;
 
 @SpringBootTest
@@ -15,6 +17,10 @@ import web.mvc.repository.MemberRepository;
 class SpringSecurityJwtApplicationTests {
   @Autowired
   private MemberRepository memberRepository;
+
+  @Autowired
+  private BoardRepository boardRepository;
+
   @Autowired
   private PasswordEncoder passwordEncoder;
   /**
@@ -24,8 +30,11 @@ class SpringSecurityJwtApplicationTests {
   void memberInsert() {
      String encPwd = passwordEncoder.encode("1234");
 
-    memberRepository.save(Member.builder().id("admin").pwd(encPwd)
+    Member member = memberRepository.save(Member.builder().id("admin").pwd(encPwd)
             .role(
     "ROLE_ADMIN").address("오리역").name("장희정").build());
+
+    boardRepository.save(Board.builder().title("test01").content("AWS 연습중01").member(member).build());
+
   }
 }
